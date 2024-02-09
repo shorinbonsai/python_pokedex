758d22ac654eae5c0c75876b3b42f1f271e210f1
James <shorinbonsai@gmail.com>
2024-02-08 23:57:44 -0500

    refactored commands and implemented help

--- pokedex/main.py
+++ pokedex/main.py
@@ -11,26 +11,10 @@ class Config:
         self.pokeapi_client = Client(timedelta(hours=0.5))
         self.caught_pokemon = {}
 
-class ConsoleCommand:
-    def __init__(self, name: str, description: str, callback: Callable[[Any], None]):
-        self.name = name
-        self.description = description
-        self.callback = callback
 
 def clean_input(text: str) -> list[str]:
     return text.lower().split()
 
-def get_commands() -> dict[str, ConsoleCommand]:
-    return {
-        "help": ConsoleCommand("help", "Displays a help message", command_help),
-        "map": ConsoleCommand("map", "List some location areas", command_map),
-        "mapb": ConsoleCommand("mapb", "List previous location areas", command_mapb),
-        "explore": ConsoleCommand("explore {location_area}", "List encountered pokemon in a location area", command_explore),
-        "catch": ConsoleCommand("catch {pokemon_name}", "Try to catch a pokemon and add to pokedex", command_catch),
-        "inspect": ConsoleCommand("inspect {pokemon_name}", "Inspect a pokemon if it exists in the pokedex", command_inspect),
-        "pokedex": ConsoleCommand("pokedex", "List all pokemon in the pokedex", command_pokedex),
-        "exit": ConsoleCommand("exit", "Exit the Pokedex", command_exit),
-    }
 
 def start(cfg, stdin, stdout):
     while True:
--- pokedex/utils.py
+++ pokedex/utils.py
@@ -1,6 +1,36 @@
+import sys
+from typing import Callable
+from typing import Any
+# from main import get_commands
+
+
+class ConsoleCommand:
+    def __init__(self, name: str, description: str, callback: Callable[[Any], None]):
+        self.name = name
+        self.description = description
+        self.callback = callback
+
+def get_commands() -> dict[str, ConsoleCommand]:
+    return {
+        "help": ConsoleCommand("help", "Displays a help message", command_help),
+        "map": ConsoleCommand("map", "List some location areas", command_map),
+        "mapb": ConsoleCommand("mapb", "List previous location areas", command_mapb),
+        "explore": ConsoleCommand("explore {location_area}", "List encountered pokemon in a location area", command_explore),
+        "catch": ConsoleCommand("catch {pokemon_name}", "Try to catch a pokemon and add to pokedex", command_catch),
+        "inspect": ConsoleCommand("inspect {pokemon_name}", "Inspect a pokemon if it exists in the pokedex", command_inspect),
+        "pokedex": ConsoleCommand("pokedex", "List all pokemon in the pokedex", command_pokedex),
+        "exit": ConsoleCommand("exit", "Exit the Pokedex", command_exit),
+    }
+
 # Placeholder functions for command callbacks
 def command_help(cfg, *args):
-    pass
+    print()
+    print("Welcome to the Pokedex.")
+    print("Here are the available commands:")
+    for name, command in get_commands().items():
+        print(f"{name}: {command.description}")
+    print()
+    return None
 
 def command_map(cfg, *args):
     pass
@@ -21,4 +51,4 @@ def command_pokedex(cfg, *args):
     pass
 
 def command_exit(cfg, *args):
-    pass
+    sys.exit(0)
